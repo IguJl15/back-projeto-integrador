1a5258515773896e2a32d59e55f4acfd56d96669
Igor julliano <igor.julliano2@gmail.com>
2023-05-30 00:59:27 -0300

    refactor: enhance errors

--- src/features/auth/domain/errors/errors.dart
+++ src/features/auth/domain/errors/errors.dart
@@ -1,16 +1,23 @@
-abstract class AuthError {
-  final String message;
+import 'dart:io';
 
-  AuthError(this.message);
+abstract class AuthError {
+  final int statusCode;
+  final String error;
+
+  AuthError(this.statusCode, this.error);
 }
 
 class AuthValidationError extends AuthError {
   final String field;
-  final String error;
+  final String detail;
 
-  AuthValidationError(this.field, this.error) : super('');
+  AuthValidationError(this.field, this.detail) : super(HttpStatus.badRequest, "Campo Inválido");
 }
 
 class UserNotFound extends AuthError {
-  UserNotFound(super.message);
+  UserNotFound(String message) : super(HttpStatus.notFound, message);
+}
+
+class EmailAlreadyInUse extends AuthError {
+  EmailAlreadyInUse() : super(HttpStatus.badRequest, "Este email ja está em uso");
 }
