a59eb8a81b7a34144843b354f0f4763fe0d2f8ca
Igor julliano <igor.julliano2@gmail.com>
2023-06-10 00:06:11 -0300

    feat: implement repository methods to save and delete tokens

--- src/features/auth/data/repositories/auth_repository_impl.dart
+++ src/features/auth/data/repositories/auth_repository_impl.dart
@@ -5,8 +5,10 @@ import '../../domain/dto/create_user_dto.dart';
 import '../../domain/models/user.dart';
 import '../../domain/repositories/auth_repository.dart';
 import '../queries/create_user_query.dart';
+import '../queries/delete_refresh_token_query.dart';
 import '../queries/get_user_by_id_query.dart';
 import '../queries/get_users_by_email_query.dart';
+import '../queries/save_refresh_token_query.dart';
 
 final class AuthRepositoryImpl implements AuthRepository {
   DatabaseConnection dbConnection;
@@ -45,5 +47,23 @@ final class AuthRepositoryImpl implements AuthRepository {
     if (dbResult.isEmpty) return null;
 
     return dbResult.first.toEntity();
+  }
+
+  @override
+  Future<void> saveRefreshToken(String jti, String userId) async {
+    final query = SaveRefreshTokenQuery(jti, userId);
+
+    await dbConnection.query(query);
+
+    return;
+  }
+
+  @override
+  Future<void> deleteToken(String jwtId) async {
+    final query = DeleteRefreshTokenQuery(jwtId);
+
+    await dbConnection.query(query);
+
+    return;
   }
 }
